8898910ea5d98e52016d592d6dd5bb89c81ce65b
ymbrother1119 <ym.brother1119@icloud.com>
2025-06-08 20:40:07 +0900

    構成作成

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Nakahara Basketball Site</h1>
+        <p>Welcome to our basketball site!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
